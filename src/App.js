275d17f996b4318e44f9619b1c5dc4f12e4e59c9
falgunmahajan <falgunmahajan1999@gmail.com>
2023-07-09 10:27:35 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react'
+import axios  from 'axios'
 import './App.css';
-
+function Table(props)
+{
+  let fetchedData=props.data;
+  let tableHeading=Object.keys(fetchedData[0]).map(key=><th>{key}</th>)
+  let tableData=fetchedData.map(item=>{
+    return<tr>{Object.keys(item).map(key=><td>{item[key]}</td>)}</tr> 
+  })
+    return(
+      <table className='table  table-bordered caption-top border-dark m-auto my-5 p-3 w-75' >
+        <caption className='text-center fw-bold fs-3 text-capitalize'>{props.state} Data</caption>
+        <thead className='align-middle'><tr>{tableHeading}</tr></thead>
+        <tbody className='align-middle'>{tableData}</tbody>
+        
+      </table>  
+    )
+}
 function App() {
+  let [state,setState]=useState('');
+  let [data,setData]=useState()
+   useEffect(()=>{
+    console.log(data)
+    if(state)
+    axios.get(`https://jsonplaceholder.typicode.com/${state}`)
+      .then(response =>setData(response.data))
+  },[state]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mx-3 my-5 App">
+      
+    <button className='me-2 btn-lg px-3' onClick={()=>setState("posts")}>Posts</button>
+    <button className='me-2 btn-lg' onClick={()=>setState("comments")}>Comments</button>
+    <button className='me-2 btn-lg' onClick={()=>setState("albums")}>Albums</button>
+    
+     {data?<Table data={data} state={state}/>:""}
     </div>
   );
 }
